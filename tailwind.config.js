08c69c35416074deb9d924ac9ab0dd5860b9b1ab
Joker 281 <dennismoldovan32@gmail.com>
2022-12-07 20:01:02 +0200

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*/*.html"],
   theme: {
-    extend: {},
+    fontFamily:{
+      noto:"Noto Sans, sans-serif"
+    },
+    screens:{
+      xs:"300px",
+      sm:"480px",
+      md:"768px",
+      lg:"976px",
+      xl:"1440px"
+    },
+    extend: {
+      colors:{
+        "softBlack":"#151515"
+      }
+    },
   },
   plugins: [],
 }
