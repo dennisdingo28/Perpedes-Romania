987c37ba64cf8826ab6b7105b619129992a757d0
Joker 281 <dennismoldovan32@gmail.com>
2022-12-11 19:21:09 +0200

    form-validation

--- js/contact.js
+++ js/contact.js
@@ -6,10 +6,12 @@ const email = document.querySelector('.email');
 const description = document.querySelector('.description');
 const submitBtn = document.querySelector('.submitBtn');
 const formStatus = document.querySelector('.formStatus');
-
-
-const deniedWords = ["matii","pula","idiot","pisat","cacat","imbecil","taran","pulii","familia","pulaa","pizda","mata","fututi","mortii","raniti","handicapatule","muie","muist","muistilor","mamele","tractorist","handicapt","pizdelor","sugi"]
-
+const wolf = document.querySelector('.wolf');
+const achilles = document.querySelector('.achilles');
+const joker = document.querySelector('.joker');
+const option = document.getElementsByTagName('option');
+console.log(option[0].value);
+const deniedWords = ["matii","pula","retardat","idiot","pisat","cacat","imbecil","taran","familia","pizda","mata","fututi","mortii","raniti","handicapatule","muie","muist","muistilor","mamele","tractorist","handicapat","pizdelor","sugi","pul","piz","sug","mui","fut","retarda","ma-ta"];
 
 founder.addEventListener('click',()=>{
     contactSelector.value = 'Wolf';
@@ -26,15 +28,24 @@ submitBtn.addEventListener('click',()=>{
 
     setTimeout(clearInput,2000);
 });
-
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
 function filterInput(){
 
     let wrongInput = false;
     if(compareInput(username) && compareInput(email) && compareInput(description)){
         validateInputs(username);
-        validateInputs(email);
+    
+        validateInputs(email)
+
         validateInputs(description);
+        
         if(checkInput(username) && checkInput(email) && checkInput(description)){
+
+           sendMail();
+            
+
             formStatus.textContent='Mesajul a fost trimis !';
             formStatus.classList.add('text-green-700');
             formStatus.classList.remove('text-red-700');
@@ -49,10 +60,6 @@ function filterInput(){
         formStatus.textContent='Mesajul tau contine injurii !'
     }
     
-    function setError(input){
-        input.nextElementSibling.nextElementSibling.classList.remove('hidden');
-        input.nextElementSibling.classList.add('hidden');
-    }
     function compareInput(input){
         const word = [];
 
@@ -81,16 +88,58 @@ function filterInput(){
     }
 }
 
-function validateInputs(input){
-    if(input.value.trim()=='')
+function sendMail(){
+    let dest;
+    if(contactSelector.value=='Wolf')
     {
-        input.nextElementSibling.nextElementSibling.classList.remove('hidden');
-        input.nextElementSibling.classList.add('hidden');
-        return false;
-    }else{  
-        input.nextElementSibling.classList.remove('hidden');
-        input.nextElementSibling.nextElementSibling.classList.add('hidden');
-        return true;
+        dest = option[0].value;
+    }
+    else if(contactSelector.value=="Achilles")
+        dest=option[1].value;
+    else
+        dest=option[2].value;
+    console.log(dest);
+
+    const params = {
+        staffMember:dest,
+        name:username.value,
+        email:email.value,
+        description:description.value
+    }
+    const serviceId = "service_stnpopm";
+    const templateId = "template_gweynbg";
+
+    emailjs.send(serviceId,templateId,params)
+    .then(res=>{
+        console.log(res);
+    }).catch(err=>{
+        console.log(err);
+    })
+}
+
+
+function validateInputs(input){
+    if(input==email){
+        if(isEmail(input.value))
+        {
+            input.nextElementSibling.classList.remove('hidden');
+            input.nextElementSibling.nextElementSibling.classList.add('hidden');
+        }
+        else{
+            input.nextElementSibling.nextElementSibling.classList.remove('hidden');
+            input.nextElementSibling.classList.add('hidden');
+        }
+    }else{
+        if(input.value.trim()=='')
+        {
+            input.nextElementSibling.nextElementSibling.classList.remove('hidden');
+            input.nextElementSibling.classList.add('hidden');
+            
+        }else{  
+            input.nextElementSibling.classList.remove('hidden');
+            input.nextElementSibling.nextElementSibling.classList.add('hidden');
+            
+        }
     }
 }
 
